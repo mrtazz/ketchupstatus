2cce3280c5b91abc885254fd43ed547af9363fde
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-28 21:41:19 +0200

    adapt server to correctly use mustaches

--- lib/server.rb
+++ lib/server.rb
@@ -5,18 +5,29 @@ require 'mustache/sinatra'
 class KetchupStatus
   class Server < Sinatra::Base
     register Mustache::Sinatra
+
     set :logging, :true
     set :public, File.dirname(__FILE__) + '/static'
-    set :views, "lib/templates/"
-    set :mustaches, "lib/views/"
 
+    set :mustache, {
+      :views     => 'lib/views/',
+      :templates => 'lib/templates/'
+    }
+
+    set :namespace, KetchupStatus::Server
+
+    def initialize(*args)
+      super
+    end
 
     get '/' do
       "Hello World!"
     end
 
     get '/:office' do
-      "Hello #{params[:office]}!"
+      @office = params[:office]
+      @image = "ketchup_empty.png"
+      mustache :status, :office => @office, :image => @image
     end
 
 
