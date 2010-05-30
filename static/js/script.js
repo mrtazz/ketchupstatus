be75296858b0b9176a5ebbc4ece17218d7408a5c
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-31 01:06:10 +0200

    update mechanism to update view

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,18 @@
 $(document).ready(function()
 {
   $("#registerbutton").click(register_office);
-
+  $("#updatebutton").click(update_status);
 
 
   function register_office()
   {
     var url = "/ketchup/"+$("#office").attr("value");
+    var res = $.post(url, {}, show_info);
+  }
+
+  function update_status()
+  {
+    var url = window.location.pathname+"/"+$("#valueinput").attr("value");
     var res = $.post(url, {}, show_info);
   }
 
