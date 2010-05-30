135d1c0aaa902c7f1a67f0b8e041367d871f05a0
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-30 16:02:05 +0200

    add ORM handling to server

--- lib/server.rb
+++ lib/server.rb
@@ -1,24 +1,26 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'mustache/sinatra'
+require 'lib/data'
 
 class KetchupStatus
   class Server < Sinatra::Base
     register Mustache::Sinatra
 
+    @base_dir = File.dirname(__FILE__) + '/../'
     set :logging, :true
-    set :public, File.dirname(__FILE__) + '/static'
+    set :public, @base_dir  + 'static'
 
     set :mustache, {
-      :views     => 'lib/views/',
-      :templates => 'lib/templates/'
+      :views     => @base_dir + 'views/',
+      :templates => @base_dir + 'templates/'
     }
 
     set :namespace, KetchupStatus::Server
 
     def initialize(*args)
       super
-      @offices = Hash.new
+      #@offices = Hash.new
     end
 
     get '/' do
@@ -26,21 +28,32 @@ class KetchupStatus
     end
 
     get '/:office' do
-      @office = params[:office]
-      @value = @offices[params[:office]]
-      @image = @offices[params[:office]]
-      mustache :status, :office => @office, :image => @image, :value => @value
+      office = KetchupStatus::Data::Office.first(:office => params[:office].to_s)
+      puts office
+      puts office.office
+      puts office.status
+      if office then
+        mustache :status, :office => office.office,
+                          :image => office.status,
+                          :value => office.status
+      else
+        404
+      end
     end
 
     post '/:office/:value' do
-      @offices[params[:office]] = params[:value]
-      "You would've set the bottle for #{params[:office]} to #{params[:value]}!"
+      office = KetchupStatus::Data::Office.first(:office => params[:office].to_s)
+      if office then
+        office.status = :value.to_i
+        office.save
+      else
+        o = KetchupStatus::Data::Office.new
+        o.office = params[:office]
+        o.status = params[:value].to_i
+        o.save
+        "You would've set the bottle for #{params[:office]} to #{params[:value]}!"
+      end
     end
-
-
-
-
-
 
   end
 end
