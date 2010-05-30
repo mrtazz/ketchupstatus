4ad6747ea3416f6c57872df5566941d13b0a23ee
Daniel Schauenberg <d@unwiredcouch.com>
2010-05-30 17:48:42 +0200

    property rename and upgrade Models on restart

--- lib/data.rb
+++ lib/data.rb
@@ -5,7 +5,6 @@ class KetchupStatus
 ### Data definitions
   module Data
 
-    base_dir = File.dirname(__FILE__) + '/../'
     DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/db/db.sqlite")
 
     class Office
@@ -14,9 +13,9 @@ class KetchupStatus
       property :id, Serial
       property :office, String
       property :status, Integer
-      property :api_key, String
+      property :token, String
     end
 
-    DataMapper.auto_migrate!
+    DataMapper.auto_upgrade!
   end
 end
